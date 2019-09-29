b0dbbfc28583f96bc0a00f9886f4d448a265abc5
Nash <18695616095@163.com>
2019-09-29 23:27:06 +0800

    调整视图结构

--- config.php
+++ config.php
@@ -4,17 +4,18 @@ return [
     'router_class' => Router::class,
     'controller_namespace' => 'App\\Controllers\\',
     'html_engine' => [
+        'delimiters' => '<!--{ }-->',
         'template_class_prefix' => '_Tpl_',
         'cache' => ROOT . '/runtime/cache/views',
         'cache_file_mode' => 0666,
         'cache_lambda_templates' => true,
-        'loader' => new Mustache_Loader_FilesystemLoader(ROOT . '/App/views/template', ['extension' => '.html']),
-        'partials_loader' => new Mustache_Loader_FilesystemLoader(ROOT . '/App/views/partials', ['extension' => '.html']),
+        'loader' => new Mustache_Loader_FilesystemLoader(ROOT . '/App/views', ['extension' => '.html']),
+        'partials_loader' => new Mustache_Loader_FilesystemLoader(ROOT . '/App/views', ['extension' => '.html']),
         'escape' => function ($val) {
             return htmlspecialchars($val, ENT_COMPAT, 'UTF-8');
         },
         'charset' => 'UTF-8',
         'strict_callables' => true,
-        'pragmas' => [Mustache_Engine::PRAGMA_FILTERS]
+        'pragmas' => [Mustache_Engine::PRAGMA_FILTERS, Mustache_Engine::PRAGMA_BLOCKS]
     ]
 ];
